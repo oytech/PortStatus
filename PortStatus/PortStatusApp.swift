2797b5fae31c1ad6cc740914b4b21beeee8b91f8
Oytech <207363903+oytech@users.noreply.github.com>
2025-05-06 16:54:40 +0200

    Change app to menu bar app without dock icon, set version to 0.1

--- PortStatus/PortStatusApp.swift
+++ PortStatus/PortStatusApp.swift
@@ -3,8 +3,15 @@ import SwiftUI
 @main
 struct PortStatusApp: App {
     var body: some Scene {
-        WindowGroup {
+        MenuBarExtra() {
             PortListView()
+            Divider()
+            Button("Quit") { NSApp.terminate(nil) }
+        } label: {
+            HStack {
+                Text("Ports")
+            }
         }
+        .menuBarExtraStyle(.window)
     }
 }

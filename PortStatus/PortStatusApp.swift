58dce048d62db85b97182e27df1df20fc0a810d7
Oytech <207363903+oytech@users.noreply.github.com>
2025-05-06 16:06:41 +0200

    Xcode generated app

---
+++ PortStatus/PortStatusApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct PortStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PortStatusTests/PortStatusTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import PortStatus
+
+struct PortStatusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

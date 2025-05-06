f134b880969289244b725efa16d18bdd061fcab8
Oytech <207363903+oytech@users.noreply.github.com>
2025-05-06 17:52:12 +0200

    PortListView dummy implementation

--- PortStatus/PortListView.swift
+++ PortStatus/PortListView.swift
@@ -1,17 +1,45 @@
 import SwiftUI
 
+struct Port: Identifiable  {
+    var name: String
+    var version: String
+    var isLatest: Bool
+
+    var id: String {
+         return "\(name)@\(version)"
+    }
+}
+
+@Observable class PortsModel {
+    var ports: [Port]
+
+    init(ports: [Port]) {
+       self.ports = ports
+    }
+}
+
 struct PortListView: View {
+    @State var model = PortsModel(ports: [])
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(model.ports) { port in
+                HStack {
+                    Text("\(port.name)")
+                    Text("\(port.version)")
+                        .background(port.isLatest ? .green : .red)
+                }
+            }
+            .listStyle(.plain)
         }
         .padding()
     }
 }
 
 #Preview {
-    PortListView()
+    let model = PortsModel(ports: [
+        Port(name: "openjdk24", version: "24.0.1", isLatest: false),
+        Port(name: "python312", version: "3.12.10", isLatest: true)
+    ])
+    PortListView(model: model)
 }
